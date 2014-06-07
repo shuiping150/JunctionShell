e1404939aee875563fa1eae283d3d93429eeb31e
shuiping150 <shuiping150@gmail.com>
2014-06-07 11:04:33 +0800

    新建项目

---
+++ JunctionShell/JunctionHelper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Monitor.Core.Utilities;
+
+namespace JunctionShell
+{
+    internal sealed class JunctionHelper
+    {
+        public void Paste()
+        {
+            var paths = Clipboard.GetFileDropList();
+            foreach (string path in paths)
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                DirectoryInfo ddir = new DirectoryInfo(Environment.CurrentDirectory + "\\" + dir.Name);
+
+                // 获取path的原始路径
+                string cur = path;
+                string next = path;
+                do 
+                {
+                    cur = next;
+                    next = JunctionPoint.GetTarget(next);
+                } while (next != null);
+
+                if (cur != path)
+                {
+                    dir = new DirectoryInfo(cur);
+                }
+
+                if (dir.Exists)
+                {
+                    if (ddir.Exists)
+                    {
+                        if (MessageBox.Show(dir.Name + "已经存在,是否覆盖?", "提问", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) != DialogResult.Yes)
+                        {
+                            continue;
+                        }
+                        ddir.Delete(true);
+                    }
+                    JunctionPoint.Create(ddir.FullName, dir.FullName, true);
+                }
+            }
+        }
+
+        public void Reg()
+        {
+            var key = Registry.ClassesRoot.OpenSubKey(@"Directory\Background\shell", true);
+            var junc = key.OpenSubKey("junction", true);
+            if (junc == null)
+            {
+                junc = key.CreateSubKey("junction");
+            }
+            junc.SetValue(string.Empty, "JunctionPaste");
+            var command = junc.OpenSubKey("Command", true);
+            if (command == null)
+            {
+                command = junc.CreateSubKey("Command");
+            }
+            command.SetValue(string.Empty, "\"" + CurrentPath + "\\" + FileName + "\" /p");
+        }
+
+        public void UnReg()
+        {
+            var key = Registry.ClassesRoot.OpenSubKey(@"Directory\Background\shell", true);
+            if (key.OpenSubKey("junction") != null)
+            {
+                key.DeleteSubKeyTree("junction");
+            }
+        }
+
+        public static string CurrentPath
+        {
+            get
+            {
+                return Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase).Replace("file:\\", "");
+            }
+        }
+
+        public static string FileName
+        {
+            get
+            {
+                return Path.GetFileName(Assembly.GetExecutingAssembly().GetName().CodeBase);
+            }
+        }
+
+    }
+}
---
+++ JunctionShell/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace JunctionShell
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            JunctionHelper helper = new JunctionHelper();
+            if (args.Length == 0)
+            {
+                helper.Reg();
+                MessageBox.Show("注册成功");
+            }
+            else
+            {
+                if (args[0].StartsWith("/"))
+                {
+                    switch (args[0].Trim().ToLower())
+                    {
+                        case "/i":
+                            helper.Reg();
+                            MessageBox.Show("注册成功");
+                    	break;
+                        case "/u":
+                            helper.UnReg();
+                            MessageBox.Show("卸载成功");
+                        break;
+                        case "/p":
+                            helper.Paste();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
